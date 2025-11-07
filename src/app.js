6443ff539075994fd2146f95e553b5c067722e15
Himanshu Jain <jainhinanshu124@gmail.com>
2025-11-07 20:02:51 +0530

    Discord bot basic setup done

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import express, { Router } from "express";
 import router from "./api/client.js";
+import client from "./bots/discord/discord.js";
 
 const PORT = process.env.PORT;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 const app = express();
 
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
+
+client.login(DISCORD_TOKEN);
 
 app.listen(PORT || 8000, () => {
 	console.log("Server running on PORT 8000");
